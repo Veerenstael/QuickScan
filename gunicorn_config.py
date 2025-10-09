b66527a0b39922b48d5291e53c689a7aa5950e29
Veerenstael Koen <abonnementen@veerenstael.nl>
2025-10-09 15:05:59 +0200

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,7 @@
 # Gunicorn configuratie voor Veerenstael QuickScan
 workers = 2
 worker_class = "sync"
-timeout = 120  # 2 minuten i.p.v. 30 seconden
+timeout = 300  # 2 minuten i.p.v. 30 seconden
 keepalive = 5
 max_requests = 1000
 max_requests_jitter = 50
